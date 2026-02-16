b93fbc95a9879e1aff3d34c99cd6604459d70fa3
fahim-shareear <fahimshareear@gmail.com>
2026-02-16 17:46:29 +0600

    redoing the ui setup

--- zap-shift-ui/src/main.jsx
+++ zap-shift-ui/src/main.jsx
@@ -1,11 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { Route, RouterProvider } from 'react-router'
-import router from './router/route'
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router}></RouterProvider>
-  </StrictMode>
+    <App />
+  </StrictMode>,
 )
