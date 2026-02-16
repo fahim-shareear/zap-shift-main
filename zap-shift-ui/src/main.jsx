968872a6c7f1911d1e6fa74b1e775470214378fb
fahim-shareear <fahimshareear@gmail.com>
2026-02-16 17:40:38 +0600

    .........

--- zap-shift-ui/src/main.jsx
+++ zap-shift-ui/src/main.jsx
@@ -2,8 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { Route, RouterProvider } from 'react-router'
+import router from './router/route'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-  </StrictMode>,
+    <RouterProvider router={router}></RouterProvider>
+  </StrictMode>
 )
