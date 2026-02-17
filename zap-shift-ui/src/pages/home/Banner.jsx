54962a574a782edcba3dfe4e5d834b4079f7e3a7
fahim-shareear <fahimshareear@gmail.com>
2026-02-18 01:33:41 +0600

    adding react-awesome-carousal and button on the carousal which is the banner

---
+++ zap-shift-ui/src/pages/home/Banner.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Carousel } from 'react-responsive-carousel';
+import bannerImg1 from '../../assets/banner/banner1.png';
+import bannerImg2 from '../../assets/banner/banner2.png';
+import bannerImg3 from '../../assets/banner/banner3.png';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { FaArrowCircleRight } from 'react-icons/fa';
+
+const Banner = () => {
+    return (
+        <Carousel autoPlay={true} interval={3000} infiniteLoop={true} className='mt-5' emulateTouch={true} dynamicHeight={true}>
+            <div className="relative">
+                <img src={bannerImg1} />
+                <div className="flex itens-center justify-center gap-4 absolute left-20 bottom-25">
+                    <button className="bg-secondary rounded-full p-3 flex items-center justify-center cursor-pointer font-bold text-xl">Track Your Percel <span className="ml-3 text-3xl -rotate-45"><FaArrowCircleRight></FaArrowCircleRight></span></button>
+                    <button className="text-xl font-semibold rounded-md p-2 cursor-pointer border border-gray-400">Be a Rider</button>
+                </div>
+            </div>
+
+            <div className="relative">
+                <img src={bannerImg2} />
+                <div className="flex itens-center justify-center gap-4 absolute left-20 bottom-25">
+                    <button className="bg-secondary rounded-full p-3 flex items-center justify-center cursor-pointer font-bold text-xl">Track Your Percel <span className="ml-3 text-3xl -rotate-45"><FaArrowCircleRight></FaArrowCircleRight></span></button>
+                    <button className="text-xl font-semibold rounded-md p-2 cursor-pointer border border-gray-400">Be a Rider</button>
+                </div>
+            </div>
+
+            <div className="relative">
+                <img src={bannerImg3} />
+                <div className="flex itens-center justify-center gap-4 absolute left-20 bottom-25">
+                    <button className="bg-secondary rounded-full p-3 flex items-center justify-center cursor-pointer font-bold text-xl">Track Your Percel <span className="ml-3 text-3xl -rotate-45"><FaArrowCircleRight></FaArrowCircleRight></span></button>
+                    <button className="text-xl font-semibold rounded-md p-2 cursor-pointer border border-gray-400">Be a Rider</button>
+                </div>
+            </div>
+        </Carousel>
+    );
+};
+
+export default Banner;
--- zap-shift-ui/src/pages/home/Home.jsx
+++ zap-shift-ui/src/pages/home/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Banner from './Banner';
 
 const Home = () => {
     return (
         <div>
-            This page is for Home
+            <Banner></Banner>
         </div>
     );
 };
