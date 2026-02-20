014465b09e5bad7ca9d19be73aed1b441290c7e8
fahim-shareear <fahimshareear@gmail.com>
2026-02-20 22:40:23 +0600

    adding the coverage component

--- zap-shift-ui/src/pages/shared/coverage/Coverage.jsx
+++ zap-shift-ui/src/pages/shared/coverage/Coverage.jsx
@@ -2,8 +2,13 @@ import React from 'react';
 
 const Coverage = () => {
     return (
-        <div>
-            
+        <div className="rounded-xl shadow-md m-5">
+            <div className="ml-4">
+                <h1 className="font-bold text-4xl text-primary">We are available in 64 districts</h1>
+            </div>
+            <div>
+                
+            </div>
         </div>
     );
 };
