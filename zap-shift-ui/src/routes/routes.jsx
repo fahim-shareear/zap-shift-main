25bdc28969ffad21aae9cbcdd9d8643a4c02bdfa
fahim-shareear <fahimshareear@gmail.com>
2026-02-20 16:57:57 +0600

    adding the coverage component

--- zap-shift-ui/src/routes/routes.jsx
+++ zap-shift-ui/src/routes/routes.jsx
@@ -1,12 +1,14 @@
 import { createBrowserRouter } from "react-router";
 import RootLayout from "../layouts/RootLayout";
 import Home from "../pages/home/Home";
+import Coverage from "../pages/shared/coverage/Coverage";
 
 const routes = createBrowserRouter([
     {
         path: '/', Component: RootLayout,
         children: [
-            {index: true, Component: Home}
+            {index: true, Component: Home},
+            {path: "coverage", Component: Coverage}
         ]
     }
 ]);
