8f575aa980f7f3384f275e8ba5c17f2f2cd7f3ce
fahim-shareear <fahimshareear@gmail.com>
2026-03-20 19:45:17 +0600

    calculating the charges for parcel type and adding a sweetalert2

--- zap-shift-ui/src/components/ParcelForm.jsx
+++ zap-shift-ui/src/components/ParcelForm.jsx
@@ -2,49 +2,89 @@ import React from 'react';
 import { useForm, useWatch } from "react-hook-form";
 import useClickAnimation from '../custonHooks/useClickAnimation';
 import { useLoaderData } from 'react-router';
+import Swal from "sweetalert2"
 
 const ParcelForm = () => {
     const { isClicked, enevtHandlers } = useClickAnimation();
-    const { handleSubmit, register, control,  formState: { errors } } = useForm();
+    const { handleSubmit, register, control, formState: { errors } } = useForm();
     const serviceCenters = useLoaderData();
     const regionsDuplicate = serviceCenters.map(c => c.region);
     const regions = [... new Set(regionsDuplicate)];
-    const senderRegion = useWatch({control, name:'senderRegion'});
-    const receiverRegion = useWatch({control, name:'receiverRegion'});
+    const senderRegion = useWatch({ control, name: 'senderRegion' });
+    const receiverRegion = useWatch({ control, name: 'receiverRegion' });
 
 
-    const districtsByRegion = region =>{
-        if(!region) return [];
+    const districtsByRegion = region => {
+        if (!region) return [];
         const regionDistricts = serviceCenters.filter(c => c.region === region);
         const districts = regionDistricts.map(d => d.district);
         return districts;
     };
-    
+
 
     const handleSendParcel = (data) => {
         console.log(data);
-        const isDocument = data.parcelType === 'document';
+        // const isDocument = data.parcelType === 'document';
+        // const isSameDistrict = data.senderDistrict === data.receiverDistrict;
+        // const parcelWeight = parseFloat(data.parcelWeight || 0);
+
+        // let cost = 0;
+        // if(isDocument){
+        //     cost = isSameDistrict ? 60 : 80;
+        // }
+        // else{
+        //     if(parcelWeight < 3){
+        //         cost = isSameDistrict ? 110 : 150;
+        //     }
+        //     else{
+        //         const minCharge = isSameDistrict ? 110 : 150;
+        //         const extraWeight = parcelWeight - 3;
+        //         const extraCharge = isSameDistrict ? extraWeight * 40 : extraWeight * 40 + 40;
+
+        //         cost = minCharge + extraCharge;
+        //     }
+        // };
+
+        // console.log("Calculated Cost:",cost);
+        const isDocument = data.parcelType === "document";
         const isSameDistrict = data.senderDistrict === data.receiverDistrict;
         const parcelWeight = parseFloat(data.parcelWeight || 0);
 
         let cost = 0;
-        if(isDocument){
+        if (isDocument) {
             cost = isSameDistrict ? 60 : 80;
         }
-        else{
-            if(parcelWeight < 3){
+        else {
+            if (parcelWeight < 3) {
                 cost = isSameDistrict ? 110 : 150;
             }
-            else{
+            else {
                 const minCharge = isSameDistrict ? 110 : 150;
                 const extraWeight = parcelWeight - 3;
                 const extraCharge = isSameDistrict ? extraWeight * 40 : extraWeight * 40 + 40;
 
                 cost = minCharge + extraCharge;
             }
-        };
+        }
 
-        console.log("Calculated Cost:",cost);
+        console.log("Cost of the parcel & delivary: ", cost);
+        Swal.fire({
+            title: "Agree with the Cost?",
+            text: `You will be charged. ${cost} taka!`,
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "I agree!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                Swal.fire({
+                    title: "Success!",
+                    text: "Your order has been canceled.",
+                    icon: "success"
+                });
+            }
+        });
     };
 
     return (
@@ -64,7 +104,7 @@ const ParcelForm = () => {
                                 value="document"
                                 className="radio"
                                 defaultChecked
-                                {...register('parcelType', {required: true})} /> 
+                                {...register('parcelType', { required: true })} />
                             Document
                         </label>
 
@@ -86,8 +126,8 @@ const ParcelForm = () => {
                             <input type="text"
                                 className="input w-full"
                                 placeholder="Parcel Name"
-                                {...register('parcelName', {required: true})} />
-                                {errors.parcelName?.type === 'required' && <p className="text-[16px] text-red-500 font-semibold">Parcel Name is required</p>}
+                                {...register('parcelName', { required: true })} />
+                            {errors.parcelName?.type === 'required' && <p className="text-[16px] text-red-500 font-semibold">Parcel Name is required</p>}
                         </fieldset>
 
                         <fieldset className="fieldset">
@@ -95,8 +135,8 @@ const ParcelForm = () => {
                             <input type="number"
                                 className="input w-full"
                                 placeholder="Parcel Weight"
-                                {...register('parcelWeight', {required: true})} />
-                                {errors.parcelWeight?.type === 'required' && <p className="text-[16px] text-red-500 font-semibold">Parcel Name is required</p>}
+                                {...register('parcelWeight', { required: true })} />
+                            {errors.parcelWeight?.type === 'required' && <p className="text-[16px] text-red-500 font-semibold">Parcel Name is required</p>}
                         </fieldset>
                     </div>
 
@@ -111,7 +151,7 @@ const ParcelForm = () => {
                                     className="input w-full"
                                     placeholder="Sender Name"
                                     {...register('senderName', { required: true })} />
-                                    {errors.senderName?.type === "required" && <p className="text-[16px] font-semibold text-red-500">Sender name field can't be empty.</p>}
+                                {errors.senderName?.type === "required" && <p className="text-[16px] font-semibold text-red-500">Sender name field can't be empty.</p>}
 
                                 {/* sender address */}
                                 <label className="label mt-4 text-bold text-primary text-[15px]">Sender Address <span className="text-red-500 text-[18px]">*</span></label>
@@ -119,7 +159,7 @@ const ParcelForm = () => {
                                     className="input w-full"
                                     placeholder="Sender address"
                                     {...register('senderAddress', { required: true })} />
-                                    {errors.senderAddress?.type === 'required' && <p className="text-[16px] font-semibold text-red-500">Sender address is a must</p>}
+                                {errors.senderAddress?.type === 'required' && <p className="text-[16px] font-semibold text-red-500">Sender address is a must</p>}
 
                                 {/* sender phone number */}
                                 <label className="label mt-4 text-bold text-primary text-[15px]">Sender Phone No <span className="text-red-500 text-[18px]">*</span></label>
@@ -127,12 +167,12 @@ const ParcelForm = () => {
                                     className="input w-full"
                                     placeholder="Sender Phone No"
                                     {...register('senderPhone', { required: true })} />
-                                    {errors.senderPhone?.type === "required" && <p className='font-semibold text-[16px] text-red-500'>Sender Contact number must be included</p>}
+                                {errors.senderPhone?.type === "required" && <p className='font-semibold text-[16px] text-red-500'>Sender Contact number must be included</p>}
 
 
                                 {/* sender region */}
                                 <label className="label mt-4 text-bold text-primary text-[15px]">Sender Region <span className="text-red-500 text-[18px]">*</span></label>
-                                <select  className="select select-md w-full" defaultValue="" {...register('senderRegion', { required: true })}>
+                                <select className="select select-md w-full" defaultValue="" {...register('senderRegion', { required: true })}>
                                     <option value={"Select Region"} >Select Region</option>
                                     {
                                         regions.map((r, index) => (
@@ -146,7 +186,7 @@ const ParcelForm = () => {
 
                                 {/* Sender district */}
                                 <label className="label mt-4 text-bold text-primary text-[15px]">Sender Region <span className="text-red-500 text-[18px]">*</span></label>
-                                <select  className="select select-md w-full" defaultValue="" {...register('senderDistrict', { required: true })}>
+                                <select className="select select-md w-full" defaultValue="" {...register('senderDistrict', { required: true })}>
                                     <option value={"Select District"} >Select District</option>
                                     {
                                         districtsByRegion(senderRegion).map((r, index) => (
@@ -177,7 +217,7 @@ const ParcelForm = () => {
                                     className="input w-full"
                                     placeholder="Receiver Name"
                                     {...register('receiverName', { required: true })} />
-                                    {errors.receiverName?.type === 'required' && <p className="font-semibold text-[16px] text-red-500">Please enter the recepients name</p>}
+                                {errors.receiverName?.type === 'required' && <p className="font-semibold text-[16px] text-red-500">Please enter the recepients name</p>}
 
                                 {/* sender address */}
                                 <label className="label mt-4 text-bold text-primary text-[15px]">Receiver Address <span className="text-red-500 text-[18px]">*</span></label>
@@ -185,7 +225,7 @@ const ParcelForm = () => {
                                     className="input w-full"
                                     placeholder="Receiver address"
                                     {...register('receiverAddress', { required: true })} />
-                                    {errors.receiverAddress?.type === "required" && <p className="font-semibold, text-[16px] text-red-500">Receipient's address must be provided</p>}
+                                {errors.receiverAddress?.type === "required" && <p className="font-semibold, text-[16px] text-red-500">Receipient's address must be provided</p>}
 
                                 {/* sender phone number */}
                                 <label className="label mt-4 text-bold text-primary text-[15px]">Receiver Phone No <span className="text-red-500 text-[18px]">*</span></label>
@@ -193,12 +233,12 @@ const ParcelForm = () => {
                                     className="input w-full"
                                     placeholder="Receiver Phone No"
                                     {...register('receiverPhone', { required: true })} />
-                                    {errors.receiverPhone?.type === 'required' && <p className="text-red-500 text-[16px] font-semibold">Please enter receivers contact number</p>}
+                                {errors.receiverPhone?.type === 'required' && <p className="text-red-500 text-[16px] font-semibold">Please enter receivers contact number</p>}
 
 
                                 {/* receiver Regions */}
                                 <label className="label mt-4 text-bold text-primary text-[15px]">Receiver Region <span className="text-red-500 text-[18px]">*</span></label>
-                                <select  className="select select-md w-full" defaultValue="" {...register('receiverRegion', { required: true })}>
+                                <select className="select select-md w-full" defaultValue="" {...register('receiverRegion', { required: true })}>
                                     <option value={"Select Region"} >Select Region</option>
                                     {
                                         regions.map((r, index) => (
@@ -211,8 +251,8 @@ const ParcelForm = () => {
                                 {errors.receiverRegion?.type === 'required' && <p className='text-[16px] text-red-500 font-semibold'>Please select the receivers Dsitrict</p>}
 
                                 {/* Receiver District */}
-                                 <label className="label mt-4 text-bold text-primary text-[15px]">Receiver District <span className="text-red-500 text-[18px]">*</span></label>
-                                <select  className="select select-md w-full" defaultValue="" {...register('receiverDistrict', { required: true })}>
+                                <label className="label mt-4 text-bold text-primary text-[15px]">Receiver District <span className="text-red-500 text-[18px]">*</span></label>
+                                <select className="select select-md w-full" defaultValue="" {...register('receiverDistrict', { required: true })}>
                                     <option value={"Select District"} >Select District</option>
                                     {
                                         districtsByRegion(receiverRegion).map((r, index) => (
@@ -238,7 +278,7 @@ const ParcelForm = () => {
                         {...enevtHandlers}
                         value="Proceed to confirm Booking"
                         className={`btn btn-secondary text-primary my-3 transition-all duration-150 transform ${isClicked ? "scale-95" : "scale-100"}`}
-                        />
+                    />
                 </form>
             </div>
         </div>
