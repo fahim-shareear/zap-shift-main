043f43002125adba566d354fcaa5e6ce9403503c
fahim-shareear <fahimshareear@gmail.com>
2026-03-23 15:39:48 +0600

    removing extra lines

--- zap-shift-ui/src/components/ParcelForm.jsx
+++ zap-shift-ui/src/components/ParcelForm.jsx
@@ -16,7 +16,7 @@ const ParcelForm = () => {
     const receiverRegion = useWatch({ control, name: 'receiverRegion' });
     const axiosSecure = useAxiosSecure();
     const {user} = useAuth();
-    console.log(user);
+    // console.log(user);
 
 
     const districtsByRegion = region => {
@@ -28,29 +28,6 @@ const ParcelForm = () => {
 
 
     const handleSendParcel = (data) => {
-        console.log(data);
-        // const isDocument = data.parcelType === 'document';
-        // const isSameDistrict = data.senderDistrict === data.receiverDistrict;
-        // const parcelWeight = parseFloat(data.parcelWeight || 0);
-
-        // let cost = 0;
-        // if(isDocument){
-        //     cost = isSameDistrict ? 60 : 80;
-        // }
-        // else{
-        //     if(parcelWeight < 3){
-        //         cost = isSameDistrict ? 110 : 150;
-        //     }
-        //     else{
-        //         const minCharge = isSameDistrict ? 110 : 150;
-        //         const extraWeight = parcelWeight - 3;
-        //         const extraCharge = isSameDistrict ? extraWeight * 40 : extraWeight * 40 + 40;
-
-        //         cost = minCharge + extraCharge;
-        //     }
-        // };
-
-        // console.log("Calculated Cost:",cost);
         const isDocument = data.parcelType === "document";
         const isSameDistrict = data.senderDistrict === data.receiverDistrict;
         const parcelWeight = parseFloat(data.parcelWeight || 0);
