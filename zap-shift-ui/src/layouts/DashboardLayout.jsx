9982f6b20f5a95710c6a4cf50a387f118e0f1a67
fahim-shareear <fahimshareear@gmail.com>
2026-03-23 19:41:52 +0600

    adding the my-parcel menu and router to the dashboard menu/navbar

--- zap-shift-ui/src/layouts/DashboardLayout.jsx
+++ zap-shift-ui/src/layouts/DashboardLayout.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Link, Outlet } from 'react-router';
+import { Link, NavLink, Outlet } from 'react-router';
+import { FaBoxArchive } from "react-icons/fa6";
 
 const DashboardLayout = () => {
     return (
@@ -12,7 +13,7 @@ const DashboardLayout = () => {
                         {/* Sidebar toggle icon */}
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" strokeLinejoin="round" strokeLinecap="round" strokeWidth="2" fill="none" stroke="currentColor" className="my-1.5 inline-block size-4"><path d="M4 4m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z"></path><path d="M9 4v16"></path><path d="M14 10l2 2l-2 2"></path></svg>
                     </label>
-                    <div className="px-4">Navbar Title</div>
+                    <div className="px-4">ZapShift Dashboard</div>
                 </nav>
                 {/* Page content here */}
                 <Outlet></Outlet>
@@ -30,6 +31,13 @@ const DashboardLayout = () => {
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" strokeLinejoin="round" strokeLinecap="round" strokeWidth="2" fill="none" stroke="currentColor" className="my-1.5 inline-block size-4"><path d="M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8"></path><path d="M3 10a2 2 0 0 1 .709-1.528l7-5.999a2 2 0 0 1 2.582 0l7 5.999A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path></svg>
                                 <span className="is-drawer-close:hidden">Homepage</span>
                             </Link>
+                        </li>
+
+                        <li>
+                            <NavLink to="/dashboard/my-parcels" className="is-drawer-close:tooltip is-drawer-close:tooltip-right" data-tip="My Parcels">
+                                <span><FaBoxArchive /></span>
+                                <span className='is-drawer-close:hidden'>My Parcels</span>
+                            </NavLink>
                         </li>
 
                         {/* List item */}
