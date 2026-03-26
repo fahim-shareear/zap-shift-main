366e9f752d4cec7d6357b73c2833af5d1bd25dc6
fahim-shareear <fahimshareear@gmail.com>
2026-03-26 18:44:29 +0600

    adding the functionality for deleting parcels from the user and adding the buttons for viewing the details, editing the details and deleting the parcel

--- zap-shift-ui/src/pages/Dashboard/MyParcels/MyParcels.jsx
+++ zap-shift-ui/src/pages/Dashboard/MyParcels/MyParcels.jsx
@@ -2,18 +2,56 @@ import React from 'react';
 import useAuth from '../../../hooks/useAuth';
 import useAxiosSecure from '../../../custonHooks/useAxiosSecure';
 import { useQuery } from '@tanstack/react-query';
+import { FaEye } from "react-icons/fa";
+import { RiEdit2Fill } from "react-icons/ri";
+import { FaTrashAlt } from "react-icons/fa";
+import Swal from 'sweetalert2';
 
 const MyParcels = () => {
     const { user } = useAuth();
     const axiosSecure = useAxiosSecure();
-    const { data: parcels = [] } = useQuery({
+    const { data: parcels = [], refetch } = useQuery({
         queryKey: ['my-parcels', user?.email],
+        enabled: !!user?.email,
         queryFn: async () => {
             const res = await axiosSecure.get(`/parcels?email=${user.email}`);
             return res.data;
         }
     });
 
+    const parcelDelete = (id) => {
+        console.log(id);
+        Swal.fire({
+            title: "Are you sure?!",
+            text: "You won't able to revert it.",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "I agree!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                axiosSecure.delete(`/parcels/${id}`)
+                    .then(res => {
+                        console.log(res.data);
+                        if (res.data.deletedCount) {
+                             //refresh the ui
+                            refetch();
+                            Swal.fire({
+                                title: "Success!",
+                                text: "Your order has been canceled.",
+                                icon: "success"
+                            });
+                           
+                        };
+                    })
+                    .catch(error =>{
+                        console.log(error.message);
+                        return
+                    });
+            };
+        });
+    }
 
     return (
         <div className="overflow-x-auto">
@@ -36,16 +74,27 @@ const MyParcels = () => {
                 <tbody>
                     {
                         parcels.map((parcel, index) => <tr key={parcel._id}>
-                        <th>{index + 1}</th>
-                        <td>{parcel.senderName}</td>
-                        <td>{parcel.parcelName}</td>
-                        <td>{parcel.cost}</td>
-                        <td>{parcel.receiverName}</td>
-                        <td>payment status</td>
-                        <td>action</td>
-                    </tr>)
+                            <th>{index + 1}</th>
+                            <td>{parcel.senderName}</td>
+                            <td>{parcel.parcelName}</td>
+                            <td>{parcel.cost}</td>
+                            <td>{parcel.receiverName}</td>
+                            <td>payment status</td>
+                            <td>
+                                <button className='btn btn-square'>
+                                    <FaEye />
+                                </button>
+
+                                <button className='btn btn-square mx-2'>
+                                    <RiEdit2Fill />
+                                </button>
+
+                                <button className='btn btn-square' onClick={() => parcelDelete(parcel._id)}>
+                                    <FaTrashAlt />
+                                </button>
+                            </td>
+                        </tr>)
                     }
-                    
                 </tbody>
             </table>
         </div>
