10b8a4051f862a8d878a743a797c1816b99585d2
fahim-shareear <fahimshareear@gmail.com>
2026-04-06 13:06:48 +0600

    creating axios interceptors for the payments api

--- zap-shift-ui/src/custonHooks/useAxiosSecure.jsx
+++ zap-shift-ui/src/custonHooks/useAxiosSecure.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import axios from 'axios';
+import { useEffect } from 'react';
+import useAuth from '../hooks/useAuth';
 
 const axiosSecure = axios.create({
     baseURL: 'http://localhost:3000'
 })
 
 const useAxiosSecure = () => {
+    const {user} = useAuth();
+
+    useEffect(()=> {
+        // intercept request:
+        axiosSecure.interceptors.request.use(config => {
+            config.headers.Authorization = `Bearer ${user?.accessToken}`
+            return config
+        })
+    }, [user])
+
     return axiosSecure;
 };
 
