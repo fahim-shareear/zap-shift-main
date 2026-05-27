62dc4949c094a11592ec7a1887d50942ba4ad5f3
fahim-shareear <fahimshareear@gmail.com>
2026-05-27 18:11:37 +0600

    adding the loader to the dashboardhome page if the role is loading it will show the loader

--- zap-shift-ui/src/pages/Dashboard/DashboardHome/DashBoardHome.jsx
+++ zap-shift-ui/src/pages/Dashboard/DashboardHome/DashBoardHome.jsx
@@ -3,9 +3,14 @@ import useRole from '../../../hooks/useRole';
 import AdminDashboardHome from './AdminDashboardHome';
 import RiderDashboardHome from './RiderDashboardHome';
 import UserDashboardHome from './UserDashboardHome';
+import Loader from '../../shared/Loader';
 
 const DashBoardHome = () => {
-    const {role} = useRole();
+    const {role, roleLoading} = useRole();
+
+    if(roleLoading){
+        return <Loader></Loader>
+    };
 
     if(role === 'admin'){
         return <AdminDashboardHome></AdminDashboardHome>
